c3a2aae319f30e0551d56053814467037a652f04
Michael Symmes <msymmes@gmail.com>
2018-08-24 09:09:47 -0400

    Changed newsApi to newsApiKey. Added sources route.

--- src/api/controllers/news.controller.js
+++ src/api/controllers/news.controller.js
@@ -1,23 +1,31 @@
 const request = require('request');
-const { newsApi } = require('../../config/vars');
+const { newsApiKey } = require('../../config/vars');
 
-const apiUrl = 'https://newsapi.org/v2';
+const apiUrl = 'https://newsApi.org/v2';
 
-// process headline requests here
-exports.getHeadlines = (req, res) => {
-  request(buildHeadlinesQuery(req.query), (err, response, body) => {
-    if (!err && response.statusCode === 200) {
-      // console.log(JSON.parse(body));
-      res.setHeader('Content-Type', 'application/json');
-      res.send(body);
-    } else {
-      console.error(err);
-      console.error(`Status Code: ${response.statusCode}`);
-      res
-        .status(response.statusCode)
-        .send('Error');
-    }
-  });
+// keep sources cached after one request
+// in the future this would need to be updated on occasion
+let sources = [];
+
+exports.getSources = (req, res) => {
+  if (sources.length === 0) {
+    request(buildSourcesQuery(), (err, response, body) => {
+      if (!err && response.statusCode === 200) {
+        ({ sources } = body);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(sources);
+      } else {
+        console.error(err);
+        console.error(`Status Code: ${response.statusCode}`);
+        res
+          .status(response.statusCode)
+          .send('Error');
+      }
+    });
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(sources);
+  }
 };
 
 // process everything searches here
@@ -37,6 +45,32 @@ exports.search = (req, res) => {
   });
 };
 
+// process headline requests here
+exports.getHeadlines = (req, res) => {
+  request(buildHeadlinesQuery(req.query), (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      // console.log(JSON.parse(body));
+      res.setHeader('Content-Type', 'application/json');
+      res.send(body);
+    } else {
+      console.error(err);
+      console.error(`Status Code: ${response.statusCode}`);
+      res
+        .status(response.statusCode)
+        .send('Error');
+    }
+  });
+};
+
+
+const buildSourcesQuery = () => {
+  const url = `${apiUrl}/sources`;
+  return {
+    url,
+    headers: { 'X-Api-Key': newsApiKey },
+  };
+
+}
 // build a query string
 // response.query => request.options
 const buildHeadlinesQuery = (query) => {
@@ -46,7 +80,7 @@ const buildHeadlinesQuery = (query) => {
   const url = apiUrl + method + page;
   return {
     url,
-    headers: { 'X-Api-Key': newsApi },
+    headers: { 'X-Api-Key': newsApiKey },
   };
 };
 
@@ -65,6 +99,6 @@ const buildSearchQuery = (query) => {
   const url = apiUrl + searchMethod + searchQuery + page + sortBy;
   return {
     url,
-    headers: { 'X-Api-Key': newsApi },
+    headers: { 'X-Api-Key': newsApiKey },
   };
 };
--- src/api/routes/v1/news.route.js
+++ src/api/routes/v1/news.route.js
@@ -5,18 +5,11 @@ const router = express.Router();
 
 router
   .route('/search')
-  /**
-   * @api {post} v1/search Search news
-   * @apiDescription Get a list of news articles related to search term
-   * @apiVersion 1.0.0
-   * @apiName Search
-   * @apiGroup Search
-   * @apiPermission None
-   *
-   * @apiSuccess {Object[]} news List of news.
-   *
-   */
   .get(controller.search);
+
+router
+  .route('/sources')
+  .get(controller.getSources);
 
 router
   .route('/headlines')
